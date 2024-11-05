71bdcbf1f05c4abf85b70fb7f609ffbbbee18a03
Ralf Junghanns <ralf.junghanns@gmail.com>
2024-11-05 12:10:57 +0100

    Add more transformers
    * DropColumnsTransformer.py
    * FillPropertyNaNsTransformer.py
    * PropertyOutlierTransformer.py

--- src/features/transformers/__init__.py
+++ src/features/transformers/__init__.py
@@ -1,9 +1,11 @@
 from .DayPhaseTransformer import DayPhaseTransformer
 from .DropColumnsTransformer import DropColumnsTransformer
 from .FillPropertyNaNsTransformer import FillPropertyNaNsTransformer
+from .PropertyOutlierTransformer import PropertyOutlierTransformer
 
 __all__ = [
     'DayPhaseTransformer',
     'DropColumnsTransformer',
-    'FillPropertyNaNsTransformer'
+    'FillPropertyNaNsTransformer',
+    'PropertyOutlierTransformer'
 ]
