8800f71eb43e5c4e0b195d2a0b1e291328f2a5d6
Mathias <masaver@gmail.com>
2024-11-09 08:57:12 +0100

    updated the StandardScaler transformer

--- src/features/transformers/StandardScalerTransformer.py
+++ src/features/transformers/StandardScalerTransformer.py
@@ -1,21 +1,23 @@
+import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.preprocessing import StandardScaler
-import pandas as pd
+
 
 class StandardScalerTransformer(BaseEstimator, TransformerMixin):
-    _columns = None
-    _scaler = None
+    _columns: list[str] | None = None
+    _types: list[str] | None = None
+    _scaler: StandardScaler | None = None
 
-    def __init__(self, columns=None):
+    def __init__(self, columns: list[str] | None = None, types: list[str] | None = None):
         """
         Initializes the ColumnScaler with the specified columns to scale.
 
         Parameters:
-        columns (list): List of columns to apply StandardScaler to. 
-        
-        MS mod: If  collumns = None, assumes all columns to be numeric, and Standarizes them
+        columns (list): List of columns to apply StandardScaler to. If None, no scaling is applied.
+        types (list, optional): List of data types of the columns. Defaults to None.
         """
-        self._columns = columns
+        self._columns = columns if columns is not None else [np.number]
+        self._types = types
         self._scaler = StandardScaler()
 
     def fit(self, X, y=None):
@@ -29,12 +31,12 @@ class StandardScalerTransformer(BaseEstimator, TransformerMixin):
         Returns:
         self: Fitted transformer.
         """
-        if self._columns is None:
-             self._scaler.fit(X)
-
         if self._columns is not None:
             self._scaler.fit(X[self._columns])
+            return self
 
+        ## If no columns are specified, fit all columns of the specified types, defaulting to all numeric columns
+        self._scaler.fit(X.select_dtypes(include=self._types))
         return self
 
     def transform(self, X):
@@ -47,13 +49,11 @@ class StandardScalerTransformer(BaseEstimator, TransformerMixin):
         Returns:
         DataFrame: Transformed DataFrame with scaled specified columns.
         """
-
         X_copy = X.copy()
-
-        if self._columns is None:
-             X_copy = pd.DataFrame( self._scaler.transform( X_copy ) , index = X_copy.index , columns = X_copy.columns )
-
         if self._columns is not None:
             X_copy[self._columns] = self._scaler.transform(X[self._columns])
+            return X_copy
 
+        ## If no columns are specified, scale all columns of the specified types, defaulting to all numeric columns
+        X_copy[X.select_dtypes(include=self._types).columns] = self._scaler.transform(X.select_dtypes(include=self._types))
         return X_copy
