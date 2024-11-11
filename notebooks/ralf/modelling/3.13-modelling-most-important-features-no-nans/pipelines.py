d39a8c1773be6acdc764aeee54b2f5c4d0a02e48
Ralf Junghanns <ralf.junghanns@gmail.com>
2024-11-11 16:22:13 +0100

    Add 3.9-3.11

---
+++ notebooks/ralf/modelling/3.13-modelling-most-important-features-no-nans/pipelines.py
@@ -0,0 +1,103 @@
+from sklearn.pipeline import Pipeline
+
+from src.features.transformers import DayPhaseTransformer, DropColumnsTransformer, ExtractColumnsTransformer, FillPropertyNaNsTransformer, GetDummiesTransformer, \
+    PropertyOutlierTransformer, StandardScalerTransformer
+
+columns_to_extract = [
+    'day_phase',
+    'bg-0:00',
+    'bg-0:15',
+    'bg-0:30',
+    'bg-0:45',
+    'bg-1:00',
+    'bg-1:15',
+    'bg-1:30',
+    'bg-1:45',
+    'bg-2:00',
+    'bg-2:15',
+    'bg-2:30',
+    'bg-2:45',
+    'bg-3:00',
+    'bg-3:15',
+    'bg-3:30',
+    'bg-3:45',
+    'bg-4:00',
+    'bg-4:15',
+    'bg-4:30',
+    'bg-4:45',
+    'bg-5:00',
+    'bg-5:15',
+    'bg-5:30',
+    'bg-5:45',
+    'insulin-0:00',
+    'insulin-0:15',
+    'insulin-0:30',
+    'insulin-0:45',
+    'insulin-1:00',
+    'insulin-1:15',
+    'insulin-1:30',
+    'insulin-1:45',
+    'insulin-2:00',
+    'insulin-2:15',
+    'insulin-2:30',
+    'insulin-2:45',
+    'insulin-3:00',
+    'insulin-3:15',
+    'insulin-3:30',
+    'insulin-3:45',
+    'insulin-4:00',
+    'insulin-4:15',
+    'insulin-4:30',
+    'insulin-4:45',
+    'insulin-5:00',
+    'insulin-5:15',
+    'insulin-5:30',
+    'insulin-5:45',
+    'cals-0:00',
+    'cals-0:15',
+    'cals-0:30',
+    'cals-0:45',
+    'cals-1:00',
+    'cals-1:15',
+    'cals-1:30',
+    'cals-1:45',
+    'cals-2:00',
+    'cals-2:15',
+    'cals-2:30',
+    'cals-2:45',
+    'cals-3:00',
+    'cals-3:15',
+    'cals-3:30',
+    'cals-3:45',
+    'cals-4:00',
+    'cals-4:15',
+    'cals-4:30',
+    'cals-4:45',
+    'cals-5:00',
+    'cals-5:15',
+    'cals-5:30',
+    'cals-5:45',
+    'bg+1:00'
+]
+
+preprocessing_pipeline = Pipeline(steps=[
+    ('day_phase', DayPhaseTransformer(time_column='time', time_format='%H:%M:%S', result_column='day_phase')),
+    ('drop_parameter_cols', DropColumnsTransformer(starts_with=['activity', 'carbs', 'steps', 'hr'])),
+    ('drop_others', DropColumnsTransformer(columns_to_delete=['p_num', 'time'])),
+    ('fill_properties_nan_bg', FillPropertyNaNsTransformer(parameter='bg', how=['interpolate'])),
+    ('fill_properties_nan_cals', FillPropertyNaNsTransformer(parameter='cals', how=['interpolate'])),
+    ('fill_properties_nan_hr', FillPropertyNaNsTransformer(parameter='hr', how=['interpolate'])),
+    ('fill_properties_nan_insulin', FillPropertyNaNsTransformer(parameter='insulin', how=['interpolate'])),
+    ('drop_outliers', PropertyOutlierTransformer(parameter='insulin', filter_function=lambda x: x < 0, fill_strategy='zero')),
+    ('extract_features', ExtractColumnsTransformer(columns_to_extract=columns_to_extract)),
+])
+
+standardization_pipeline = Pipeline(steps=[
+    ('get_dummies', GetDummiesTransformer(columns=['day_phase'])),
+    ('min_max_scaler', StandardScalerTransformer(columns=columns_to_extract[1:-1]))
+])
+
+pipeline = Pipeline(steps=[
+    ('preprocessing', preprocessing_pipeline),
+    ('standardization', standardization_pipeline)
+])
