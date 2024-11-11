87278eaa740f978417763b6f77caf73ad93001f4
Mathias <masaver@gmail.com>
2024-11-11 15:42:33 +0100

    updated XGB tuner

--- src/features/tuners/XGBHyperparameterTuner.py
+++ src/features/tuners/XGBHyperparameterTuner.py
@@ -3,31 +3,33 @@ import pandas as pd
 from matplotlib import pyplot as plt
 from skopt import BayesSearchCV
 from xgboost import XGBRegressor
+from skopt.space import Integer, Real
+import shap
 from sklearn.metrics import root_mean_squared_error, r2_score, PredictionErrorDisplay
 
 param_spaces = {
     'default': {
-        'n_estimators': (100, 300),  # Common range for boosting rounds
-        'learning_rate': (0.03, 0.1, 'log-uniform'),  # Standard learning rates
-        'max_depth': (3, 7),  # Default depth range that balances depth and overfitting
-        'min_child_weight': (1, 5),  # Moderate range to allow small variations in regularization
-        'subsample': (0.7, 0.85),  # Light subsampling for diversity while preserving data
-        'colsample_bytree': (0.7, 0.85),  # Common column sampling range
-        'gamma': (0, 3),  # Small gamma range for light regularization
-        'alpha': (1e-3, 0.1, 'log-uniform'),  # Default L1 regularization
-        'lambda': (1e-3, 0.1, 'log-uniform'),  # Default L2 regularization
+        'n_estimators': Integer(100, 300),  # Common range for boosting rounds
+        'learning_rate': Real(0.03, 0.1, 'log-uniform'),  # Standard learning rates
+        'max_depth': Integer(3, 7),  # Default depth range that balances depth and overfitting
+        'min_child_weight': Integer(1, 5),  # Moderate range to allow small variations in regularization
+        'subsample': Real(0.7, 0.85),  # Light subsampling for diversity while preserving data
+        'colsample_bytree': Real(0.7, 0.85),  # Common column sampling range
+        'gamma': Integer(0, 3),  # Small gamma range for light regularization
+        'alpha': Real(1e-3, 0.1, 'log-uniform'),  # Default L1 regularization
+        'lambda': Real(1e-3, 0.1, 'log-uniform'),  # Default L2 regularization
     },
     'deep': {
-        'n_estimators': (50, 1000),  # Number of boosting rounds
-        'learning_rate': (0.001, 0.3, 'log-uniform'),  # Step size shrinkage, log-uniform for finer control
-        'max_depth': (3, 15),  # Maximum tree depth for base learners
-        'min_child_weight': (1, 10),  # Minimum sum of instance weight (hessian) needed in a child
-        'subsample': (0.5, 1.0),  # Subsample ratio of the training instances
-        'colsample_bytree': (0.3, 1.0),  # Subsample ratio of columns when constructing each tree
-        'gamma': (0, 10),  # Minimum loss reduction required to make a further partition on a leaf node
-        'alpha': (1e-10, 10.0, 'log-uniform'),  # L1 regularization term on weights
-        'lambda': (1e-10, 10.0, 'log-uniform'),  # L2 regularization term on weights
-        'scale_pos_weight': (1, 100),  # Controls balance of positive and negative weights, often used for imbalanced classes
+        'n_estimators': Integer(50, 1000),  # Number of boosting rounds
+        'learning_rate': Real(0.001, 0.3, 'log-uniform'),  # Step size shrinkage, log-uniform for finer control
+        'max_depth': Integer(3, 15),  # Maximum tree depth for base learners
+        'min_child_weight': Integer(1, 10),  # Minimum sum of instance weight (hessian) needed in a child
+        'subsample': Real(0.5, 1.0),  # Subsample ratio of the training instances
+        'colsample_bytree': Real(0.3, 1.0),  # Subsample ratio of columns when constructing each tree
+        'gamma': Integer(0, 10),  # Minimum loss reduction required to make a further partition on a leaf node
+        'alpha': Real(1e-10, 10.0, 'log-uniform'),  # L1 regularization term on weights
+        'lambda': Real(1e-10, 10.0, 'log-uniform'),  # L2 regularization term on weights
+        'scale_pos_weight': Integer(1, 100),  # Controls balance of positive and negative weights, often used for imbalanced classes
     },
     'fast': {
         'n_estimators': [100, 200],  # Limited boosting rounds for quick training
@@ -49,11 +51,37 @@ class XGBHyperparameterTuner:
     _y_pred: pd.Series | None
     __name__ = 'XGBRegressor'
 
-    def __init__(self, search_space='default'):
+    def __init__(self,  n = None , search_space = 'default' ):
+        self._n = n
         self._search_space = param_spaces[search_space] if search_space in param_spaces.keys() else param_spaces['default']
 
     def fit(self, X, y):
-        np.int = int
+        # np.int = int
+        
+        # Fit athe base model
+        estimator = XGBRegressor(objective='reg:squarederror', random_state=42, tree_method='hist')
+        estimator.fit(X,y)
+
+        # # Do a SHAP analysis
+        explainer = shap.Explainer( estimator )
+        shap_values = explainer( X )
+        self._shap_values = shap_values
+
+        # Get the Top n best features
+        mean_abs_shap_values = np.abs( self._shap_values.values ).mean(axis=0)
+        feature_importance_df = pd.DataFrame({
+            'feature': X.columns, 
+            'mean_abs_shap_value': mean_abs_shap_values
+        })
+
+        feature_importance_df = feature_importance_df.sort_values(by='mean_abs_shap_value', ascending=False)
+        
+        # Get the top-N features (for example, top 10 features)
+        if self._n is not None:
+            feature_importance_df = feature_importance_df.head( self._n )
+            self.top_n_features = list( feature_importance_df['feature'] )
+
+        #
         search_cv = BayesSearchCV(
             estimator=XGBRegressor(objective='reg:squarederror', random_state=42, tree_method='hist'),
             search_spaces=self._search_space,
@@ -63,16 +91,24 @@ class XGBHyperparameterTuner:
             n_jobs=-1,
             random_state=42
         )
-        search_cv.fit(X=X, y=y)
+        
+        if self._n is not None:
+            search_cv.fit( X = X[self.top_n_features] , y = y )
+            regressor = XGBRegressor(**search_cv.best_params_, objective='reg:squarederror', random_state=42, tree_method='hist')
+            regressor.fit(X=X[self.top_n_features], y=y)
+            self._y_pred = regressor.predict(X=X[self.top_n_features])
 
-        regressor = XGBRegressor(**search_cv.best_params_, objective='reg:squarederror', random_state=42, tree_method='hist')
-        regressor.fit(X=X, y=y)
+        
+        if self._n is None:
+            search_cv.fit( X = X , y = y )
+            regressor = XGBRegressor(**search_cv.best_params_, objective='reg:squarederror', random_state=42, tree_method='hist')
+            regressor.fit(X=X, y=y)
+            self._y_pred = regressor.predict(X=X)
 
         self._best_model = regressor
         self._best_params = search_cv.best_params_
         self._y_train = y
-        self._y_pred = regressor.predict(X=X)
-
+        
     def get_params(self):
         if self._best_params is None:
             raise ValueError('No model has been fitted yet')
@@ -93,6 +129,11 @@ class XGBHyperparameterTuner:
         if self._best_model is None:
             raise ValueError('No model has been fitted yet')
         return self._best_model
+    
+    def get_best_features(self):
+        if self._n is None:
+            raise ValueError('No features selected based on SHAP')
+        return self.top_n_features
 
     def show_chart(self):
         if self._best_model is None:
@@ -120,3 +161,13 @@ class XGBHyperparameterTuner:
         fig.suptitle("Plotting cross-validated predictions")
         plt.tight_layout()
         plt.show()
+
+    def show_shap(self):
+        if self._best_model is None:
+            raise ValueError('No model has been fitted yet')
+        
+        if self._n is not None:
+            shap.plots.beeswarm( self._shap_values[:,self.top_n_features] , max_display = len( self.top_n_features ) )
+        
+        if self._n is None:
+            shap.plots.beeswarm( self._shap_values[:,self.top_n_features] )
