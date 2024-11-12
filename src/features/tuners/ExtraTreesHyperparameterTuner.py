00ae4c8e9330c167244b784a08276840179fd961
Ralf Junghanns <ralf.junghanns@gmail.com>
2024-11-12 10:33:39 +0100

    Add LGBMHyperparameterTuner.py

--- src/features/tuners/ExtraTreesHyperparameterTuner.py
+++ src/features/tuners/ExtraTreesHyperparameterTuner.py
@@ -1,5 +1,5 @@
 from sklearn.ensemble import ExtraTreesRegressor
-from skopt.space import Integer, Real
+from skopt.space import Integer, Real, Categorical
 
 from src.features.tuners.BaseHyperparameterTuner import BaseHyperparameterTuner
 
@@ -14,6 +14,17 @@ param_spaces = {
         'max_leaf_nodes': Integer(15, 31),  # Maximum number of leaves in each tree
         'min_impurity_decrease': Real(0, 0.1),  # Minimum impurity decrease to split a node
         'random_state': [42],  # Random seed for reproducibility
+    },
+    'deep': {
+        'n_estimators': Integer(100, 1000),  # Wider range for number of trees
+        'max_depth': Integer(5, 50),  # Deeper trees if desired
+        'min_samples_split': Integer(2, 50),  # Higher minimum split values
+        'min_samples_leaf': Integer(1, 50),  # Higher range for min samples at leaf
+        'max_features': Categorical(['sqrt', 'log2', None]),  # Standard feature options
+        'bootstrap': Categorical([True, False]),  # Bootstrap sampling option
+        'max_leaf_nodes': Integer(10, 1000),  # Maximum number of leaf nodes per tree
+        'min_impurity_decrease': Real(0.0, 0.5, 'uniform'),  # Minimum impurity decrease for splitting
+        'ccp_alpha': Real(0.0, 0.1, 'uniform')  # Complexity parameter for Minimal Cost-Complexity Pruning
     }
 }
 
