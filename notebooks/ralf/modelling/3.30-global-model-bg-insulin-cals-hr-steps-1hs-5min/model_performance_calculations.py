c1c584b7710b550788ee5ae9a062ce643de6c71f
Ralf Junghanns <ralf.junghanns@gmail.com>
2024-11-26 16:28:51 +0100

    Add meta-estimator

--- notebooks/ralf/modelling/3.30-global-model-bg-insulin-cals-hr-steps-1hs-5min/model_performance_calculations.py
+++ notebooks/ralf/modelling/3.30-global-model-bg-insulin-cals-hr-steps-1hs-5min/model_performance_calculations.py
@@ -9,7 +9,9 @@ import numpy as np
 from jedi.inference.gradual.typing import TypedDict
 from sklearn.ensemble import StackingRegressor
 from sklearn.model_selection import ShuffleSplit
-from sklearn.metrics import root_mean_squared_error, mean_absolute_error, mean_squared_error
+from sklearn.metrics import root_mean_squared_error, mean_absolute_error, mean_squared_error, r2_score
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.callbacks import EarlyStopping
 
 
 class ModelScoreParameter(TypedDict):
@@ -68,7 +70,7 @@ def print_conditionally(message, verbose=True):
         print(f'{get_date_time_now()} - {message}')
 
 
-def calculate_stacking_regressor_performance(model: StackingRegressor, X_train, y_train, X_additional_train, y_additional_train, verbose=True):
+def calculate_stacking_regressor_performance(model: StackingRegressor, X_train, y_train, X_additional_train, y_additional_train, verbose=True, n_splits=5):
     print_conditionally(f'{get_date_time_now()} - Start training', verbose)
 
     models = model.estimators + [('final_estimator', model.final_estimator), ('stacking_regressor', model)]
@@ -80,7 +82,7 @@ def calculate_stacking_regressor_performance(model: StackingRegressor, X_train, 
         score = {}
         split_index = 0
         print_conditionally(f'{get_date_time_now()} - Splitting the additional train data with ShuffleSplit')
-        splitter = ShuffleSplit(n_splits=10, test_size=0.2, random_state=42)
+        splitter = ShuffleSplit(n_splits=n_splits, test_size=0.2, random_state=42)
         for train_index, test_index in splitter.split(X_additional_train):
             split_index += 1
             print_conditionally(f'{get_date_time_now()} - Split {split_index} - Model: {model_name}', verbose)
@@ -126,6 +128,68 @@ def calculate_stacking_regressor_performance(model: StackingRegressor, X_train, 
         model_scores.append(model_score)
 
     return model_scores
+
+
+def calculate_dnn_performance(model: Sequential, X_train, y_train, X_additional_train, y_additional_train, verbose=True, n_splits=5, epochs=50):
+    print_conditionally(f'Start training DNN', verbose)
+
+    if verbose:
+        model.summary()
+
+    score = {}
+    splitter = ShuffleSplit(n_splits=n_splits, test_size=0.2, random_state=42)
+    split_index = 0
+    for train_index, test_index in splitter.split(X_additional_train):
+        split_index += 1
+        print_conditionally(f'Split {split_index}/{n_splits}', verbose)
+        X_train_split, X_test_split = X_additional_train.iloc[train_index], X_additional_train.iloc[test_index]
+        y_train_split, y_test_split = y_additional_train.iloc[train_index], y_additional_train.iloc[test_index]
+
+        early_stop = EarlyStopping(
+            monitor='val_loss',
+            patience=10,
+            restore_best_weights=True
+        )
+
+        model.fit(
+            pd.concat([X_train, X_train_split]),
+            pd.concat([y_train, y_train_split]),
+            validation_data=(X_test_split, y_test_split),
+            epochs=epochs,
+            callbacks=[early_stop],
+            verbose=2
+        )
+
+        y_pred = model.predict(X_test_split)
+
+        r_squared = r2_score(y_test_split, y_pred)
+        rmse = root_mean_squared_error(y_test_split, y_pred)
+        mae = mean_absolute_error(y_test_split, y_pred)
+        mse = mean_squared_error(y_test_split, y_pred)
+
+        score['r_squared'] = score.get('r_squared', []) + [r_squared]
+        score['rmse'] = score.get('rmse', []) + [rmse]
+        score['mae'] = score.get('mae', []) + [mae]
+        score['mse'] = score.get('mse', []) + [mse]
+
+        print_conditionally(f'R^2: {r_squared}, RMSE: {rmse}, MAE: {mae}, MSE: {mse}', verbose)
+
+    print_conditionally('Training finished', verbose)
+
+    score = {k: {
+        'mean': np.mean(v),
+        'std': np.std(v),
+        'min': np.min(v),
+        'max': np.max(v),
+        'values': v
+    } for k, v in score.items()}
+
+    model_score = ModelScore('DNN')
+    model_score.r_squared = score['r_squared']
+    model_score.rmse = score['rmse']
+    model_score.mae = score['mae']
+    model_score.mse = score['mse']
+    return model_score
 
 
 def get_rmse_boxplot_chart(scores: list[ModelScore] | ModelScore):
