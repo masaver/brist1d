bdd3b24e09dc216ce007ea0233e621ed5d2fe68a
Ralf Junghanns <ralf.junghanns@gmail.com>
2024-11-27 12:02:16 +0100

    Update Models

--- notebooks/ralf/modelling/3.30-global-model-bg-insulin-cals-hr-steps-1hs-5min/model_performance_calculations.py
+++ notebooks/ralf/modelling/3.30-global-model-bg-insulin-cals-hr-steps-1hs-5min/model_performance_calculations.py
@@ -8,7 +8,7 @@ import pandas as pd
 import numpy as np
 from jedi.inference.gradual.typing import TypedDict
 from sklearn.ensemble import StackingRegressor
-from sklearn.model_selection import ShuffleSplit
+from sklearn.model_selection import ShuffleSplit, LeaveOneGroupOut
 from sklearn.metrics import root_mean_squared_error, mean_absolute_error, mean_squared_error, r2_score
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.callbacks import EarlyStopping
@@ -70,34 +70,40 @@ def print_conditionally(message, verbose=True):
         print(f'{get_date_time_now()} - {message}')
 
 
-def calculate_stacking_regressor_performance(model: StackingRegressor, X_train, y_train, X_additional_train, y_additional_train, verbose=True, n_splits=5):
-    print_conditionally(f'{get_date_time_now()} - Start training', verbose)
+def calculate_stacking_regressor_performance(model: StackingRegressor, X_train, y_train, X_additional_train, y_additional_train, verbose=True, n_splits=5, groups=None):
+    print_conditionally(f'Start training', verbose)
 
     models = model.estimators + [('final_estimator', model.final_estimator), ('stacking_regressor', model)]
     model_scores = []
 
     for model_name, model in models:
-        print_conditionally(f'{get_date_time_now()} - Calculate performance for {model_name}', verbose)
+        print_conditionally(f'Calculate performance for {model_name}', verbose)
+
+        splitter = ShuffleSplit(n_splits=n_splits, test_size=0.2, random_state=42)
+        if groups is not None:
+            splitter = LeaveOneGroupOut()
+            print(f'Number of splits: {splitter.get_n_splits(groups=groups)}')
+
+        print_conditionally(f'Selected splitter: {splitter}', verbose)
 
         score = {}
         split_index = 0
-        print_conditionally(f'{get_date_time_now()} - Splitting the additional train data with ShuffleSplit')
-        splitter = ShuffleSplit(n_splits=n_splits, test_size=0.2, random_state=42)
-        for train_index, test_index in splitter.split(X_additional_train):
+        for train_index, test_index in splitter.split(X_additional_train, groups=groups):
             split_index += 1
-            print_conditionally(f'{get_date_time_now()} - Split {split_index} - Model: {model_name}', verbose)
+            print_conditionally(f'Split {split_index} - Model: {model_name}', verbose)
 
             X_train_split, X_test_split = X_additional_train.iloc[train_index], X_additional_train.iloc[test_index]
             y_train_split, y_test_split = y_additional_train.iloc[train_index], y_additional_train.iloc[test_index]
 
-            print_conditionally(f'{get_date_time_now()} - Fitting the model')
+            print_conditionally(f'Fitting the model')
             model.fit(pd.concat([X_train, X_train_split]), pd.concat([y_train, y_train_split]))
 
-            print_conditionally(f'{get_date_time_now()} - Predicting')
+            print_conditionally(f'Predicting')
             y_pred = model.predict(X_test_split)
 
-            print_conditionally(f'{get_date_time_now()} - Calculating scores')
-            r_squared = model.score(X_test_split, y_test_split)
+            print_conditionally(f'Calculating scores')
+
+            r_squared = r2_score(y_test_split, y_pred)
             rmse = root_mean_squared_error(y_test_split, y_pred)
             mae = mean_absolute_error(y_test_split, y_pred)
             mse = mean_squared_error(y_test_split, y_pred)
@@ -107,9 +113,9 @@ def calculate_stacking_regressor_performance(model: StackingRegressor, X_train, 
             score['mae'] = score.get('mae', []) + [mae]
             score['mse'] = score.get('mse', []) + [mse]
 
-            print_conditionally(f'{get_date_time_now()} - R^2: {r_squared}, RMSE: {rmse}, MAE: {mae}, MSE: {mse}', verbose)
+            print_conditionally(f'R^2: {r_squared}, RMSE: {rmse}, MAE: {mae}, MSE: {mse}', verbose)
 
-        print_conditionally(f'{get_date_time_now()} - Training finished', verbose)
+        print_conditionally(f'Training finished', verbose)
 
         score = {k: {
             'mean': np.mean(v),
@@ -130,16 +136,21 @@ def calculate_stacking_regressor_performance(model: StackingRegressor, X_train, 
     return model_scores
 
 
-def calculate_dnn_performance(model: Sequential, X_train, y_train, X_additional_train, y_additional_train, verbose=True, n_splits=5, epochs=50):
+def calculate_dnn_performance(model: Sequential, X_train, y_train, X_additional_train, y_additional_train, verbose=True, n_splits=5, epochs=50, groups=None):
     print_conditionally(f'Start training DNN', verbose)
 
     if verbose:
         model.summary()
 
-    score = {}
     splitter = ShuffleSplit(n_splits=n_splits, test_size=0.2, random_state=42)
+    if groups is not None:
+        splitter = LeaveOneGroupOut()
+
+    print_conditionally(f'Selected splitter: {splitter}', verbose)
+
+    score = {}
     split_index = 0
-    for train_index, test_index in splitter.split(X_additional_train):
+    for train_index, test_index in splitter.split(X=X_additional_train, groups=groups):
         split_index += 1
         print_conditionally(f'Split {split_index}/{n_splits}', verbose)
         X_train_split, X_test_split = X_additional_train.iloc[train_index], X_additional_train.iloc[test_index]
@@ -197,6 +208,10 @@ def get_rmse_boxplot_chart(scores: list[ModelScore] | ModelScore):
         scores = [scores]
 
     final_estimator_score = scores[-1]
+    print(f'Final estimator RMSE: {final_estimator_score.rmse["mean"]}')
+    print(f'Final estimator R2: {final_estimator_score.r_squared["mean"]}')
+    print(f'Final estimator MSE: {final_estimator_score.mse["mean"]}')
+    print(f'Final estimator MAE: {final_estimator_score.mae["mean"]}')
     values = []
     labels = []
     for score in scores:
