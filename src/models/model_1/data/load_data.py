ee8a01a72ea31818380b827707b2298f450a845c
Mathias <masaver@gmail.com>
2024-12-03 16:59:20 +0100

    fixed load_data.py

--- src/models/model_1/data/load_data.py
+++ src/models/model_1/data/load_data.py
@@ -20,28 +20,25 @@ def load_data(recreate: bool = False) -> tuple[pd.DataFrame, pd.Series, pd.DataF
 
     train_data, augmented_data, test_data = common_load_data('1_00h', True)
 
-    # Split feature and target variables
-    xtrain = train_data.drop( 'bg+1:00' , axis = 1 )
-    ytrain = train_data['bg+1:00']
-
-    # Apply preprocessing pipeline
-    data_pipe = pipeline
-    Xs = data_pipe.fit_transform( xtrain )
-    Xs_test = data_pipe.transform( test_data )
+    # Transform the data and split feaures and target variables
+    train_data_transformed = pipeline.fit_transform( train_data )
+    X_train = train_data_transformed.drop( 'bg+1:00' , axis = 1 )
+    y_train = train_data['bg+1:00']
+    X_test = pipeline.fit_transform(test_data)
 
     # fix column names - Needed for LGBM
     import re
-    Xs.columns = [re.sub(r'[^a-zA-Z0-9_]', '_', col) for col in Xs.columns]
-    Xs_test.columns = [re.sub(r'[^a-zA-Z0-9_]', '_', col) for col in Xs_test.columns]
+    X_train.columns = [re.sub(r'[^a-zA-Z0-9_]', '_', col) for col in X_train.columns]
+    X_test.columns = [re.sub(r'[^a-zA-Z0-9_]', '_', col) for col in X_test.columns]
 
     #Subset  to keep top features opnly
     top_feat = ['hr_0_00', 'bg_0_15', 'day_phase_evening', 'bg_0_00', 'insulin_0_00', 'day_phase_night', 'bg_0_10']
-    Xs = Xs[ top_feat ]
-    Xs_test = Xs_test[ top_feat ]
+    X_train = X_train[ top_feat ]
+    X_test = X_test[ top_feat ]
 
     # Save the processed data
-    Xs.to_csv(filename_x_train)
-    ytrain.to_csv(filename_y_train)
-    Xs_test.to_csv(filename_x_test)
+    X_train.to_csv(filename_x_train)
+    y_train.to_csv(filename_y_train)
+    X_test.to_csv(filename_x_test)
 
-    return Xs, ytrain, Xs_test
+    return X_train , y_train , X_test
